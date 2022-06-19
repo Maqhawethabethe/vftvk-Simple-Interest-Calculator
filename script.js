d6975617690cc7f270de3e7dcc3f066df286fdf0
maqhawe thabethe <qhawes54@gmail.com>
2022-06-19 00:08:37 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+var slider=document.getElementById("myRange");
+var val = document.getElementById("value");
+
+//console.log(slider.value)
+
+slider.addEventListener("input",()=>{
+    console.log(slider.value)
+    val.innerText = slider.value+"%"
+})
+
+
+var principal = document.getElementById("principal").value;
+var rate = document.getElementById("rate").value;
+var years = document.getElementById("years").value;
+var interest = principal * years * myRange /100;
+var year = new Date().getFullYear()+parseInt(years);
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   alert("HI");
+   
 }
-        
+
+function updateRate() 
+{
+    var rateval = document.getElementById("myRange").value;
+    document.getElementById("rate_val").innerText=rateval;
+} 
